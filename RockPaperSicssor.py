77a6b826ca95cee3095ca51eac0b0f057c3928e9
ranjanlamsal <58652506+ranjanlamsal@users.noreply.github.com>
2020-09-03 18:20:00 +0545

    Update RockPaperSicssor.py

--- RockPaperSicssor.py
+++ RockPaperSicssor.py
@@ -1,20 +1,19 @@
 import random
-comp_points = 0
-usr_points = 0
-print('''Welcome Rock Paper Scissor game.
- You have to type 'p' for paper,
- 's' for Scissor and 'r' for rock.
- There will be total of 10 rounds and results will be displayed.
- Type "exit" if you want to terminate the game\v''')
-li = ["r", "p", "s"]
-i = 0
+chance = 10
 while True:
-    while i < 10:
+    print('''Welcome Rock Paper Scissor game.
+     You have to type 'p' for paper,
+     's' for Scissor and 'r' for rock.
+     There will be total of 10 rounds and results will be displayed.
+     Type "exit" if you want to terminate the game\v''')
+    li = ["r", "p", "s"]
+    comp_points = 0
+    usr_points = 0
+    i = 0
+    while i < chance:
         usr_choice = input("'p' or 's' or 'r' ?\n>>").lower()
         rand = random.choice(li)
         if usr_choice in li or "exit":
-            # global comp_points
-            # global usr_points
             if usr_choice == rand:
                 print(f"Draw.\t\t{9-i} rounds left\n")
                 i = i + 1
@@ -42,21 +41,24 @@ while True:
                 print(f"Computer +1 point\t\t{9 - i} rounds left\n")
                 i = i + 1
                 comp_points = comp_points + 1
-            else:
-                break
+            # else:
+            #     break
         else:
             print('''no command exist. You have to type 'p' for paper,
                 's' for Scissor and 'r' for rock. To terminate type 'exit'.''')
             continue
-    print("Game Completed.")
-    if usr_points > comp_points:
-        print(f"Congratulation you won\n Your point: {usr_points}\nComputer points:{comp_points}")
-    elif usr_points == comp_points:
-        print(f"Game Draw!\nYour point: {usr_points}\nComputer points:{comp_points}")
-    else:
-        print(f"Sorry you lose.\nYour point: {usr_points}\nComputer points:{comp_points}")
+        if i == 10:
+            print("Game Completed.")
+            if usr_points > comp_points:
+                print(f"Congratulation you won\n Your point: {usr_points}\nComputer points:{comp_points}")
+            elif usr_points == comp_points:
+                print(f"Game Draw!\nYour point: {usr_points}\nComputer points:{comp_points}")
+            else:
+                print(f"Sorry you lose.\nYour point: {usr_points}\nComputer points:{comp_points}")
+
     conti = input("Do you want to continue?('y' for yes)").lower()
     if conti == "y":
+        i = 0
         continue
     else:
         break
